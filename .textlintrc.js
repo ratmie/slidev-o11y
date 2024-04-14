b3a913be21f9f114e1b9ae5945a1ca1d29a07d20
Takegata <39664958+ratmie@users.noreply.github.com>
2024-04-14 01:05:48 +0000

    slidevの導入、目次の作成

---
+++ .textlintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        "no-mix-dearu-desumasu": true,
+        "no-double-negative-ja": true,
+        "no-hankaku-kana": true,
+        "no-dropping-the-ra": true,
+        "ja-no-mixed-period": true,
+        "prefer-tari-tari": true,
+        "@textlint-ja/no-synonyms": true,
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # slidev-o11y
+
+スライドのリポジトリ
+- slidevつかってみる
+- o11yことはじめ
+
+## codespacesだと
+
+bunが入っていないので
+```
+npm install -g bun 
+```
